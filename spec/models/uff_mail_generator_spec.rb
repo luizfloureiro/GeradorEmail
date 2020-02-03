5742e452255e5c7229e5faea0b76291e3df1b088
luizfloureiro <luizfcloureiro@gmail.com>
2020-02-03 17:19:38 -0300

    Alteração no teste do UffMailGenerator.

--- spec/models/uff_mail_generator_spec.rb
+++ spec/models/uff_mail_generator_spec.rb
@@ -5,7 +5,7 @@ describe 'UffMailGenerator', '#opcoes' do
   context "ao ser instanciado com um nome" do
     it "cria 5 opcoes para o uffmail" do
       opcoes = UffMailGenerator.new("Nome Usuario").gerar
-      expect(opcoes).to eq %w(nomeusuario@uffmail.com nome.usuario@uffmail.com nome_usuario@uffmail.com n.usuario@uffmail.com nome.u@uffmail.com)
+      expect(opcoes).to match_array %w(nomeusuario@uffmail.com nome.usuario@uffmail.com nome_usuario@uffmail.com n.usuario@uffmail.com nome.u@uffmail.com)
     end
   end
 
